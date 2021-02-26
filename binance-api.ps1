5d78f32eeab228010f8f98492441e600e4410304
PCMaddin <pcmaddin@gmail.com>
2021-02-27 00:09:21 +0100

    Update binance-api.ps1
    more examples

--- binance-api.ps1
+++ binance-api.ps1
@@ -12,8 +12,19 @@ $depth = $bt.depth("ETHBTC")
 $trades =$bt.trades("ETHBTC")
 $exchangeinfo =$bt.Exchange_Info()
 $ratelimits=$exchangeinfo.rateLimits
-$bt.ticker24hr("ETHBTC")
-
+$allticker=$bt.ticker24hr()
 Write-Output (new-timespan -Start $starttime -End $(Get-Date)) #for measure runtime
 
 Pause(30)
+
+
+##EXAMPLE FOR MARKET CAP FILTER IN BOTS : copy to new script and uncomment
+
+#Using module .\binance-api.psm1
+#$global:progressPreference = 'silentlyContinue' #Better performance and looks better 
+#$key="YOUR_API_KEY"   #<---- NEEEED
+#$secret="YOUR_API_SECRET"    #<---- NEEEED
+#$bt= [Binance]::new($key,$secret)
+#$allticker=$bt.ticker24hr()
+#$bridge="BUSD"
+#($allticker| Where{ $_.symbol -like "*$bridge"  }| Where { $( [double]$_.quoteVolume) -gt 5000000}).symbol.replace("$bridge","") | Out-File supported_coinlist.txt
