6274a8ab95487019fc40e7783e36c77d60331cbd
PCMaddin <pcmaddin@gmail.com>
2022-12-09 19:03:09 +0100

    Update binance-api.psm1
    now with working new limit order

--- binance-api.psm1
+++ binance-api.psm1
@@ -3,7 +3,8 @@ class Binance {
  [String] $ApiSecret
  [String] $ApiKey
  [long] $timediff
-  Binance([String]$newApiKey,[String]$newApiSecret){
+
+ Binance([String]$newApiKey,[String]$newApiSecret){
   $this.Set_ApiCredentials($newApiKey,$newApiSecret)
   $this.choose_ApiServer() 
  [long]$servertime=$this.Get_Time()
@@ -16,26 +17,44 @@ class Binance {
 [long]$mytime=((New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds)
  [int]$this.timediff =$mytime - $servertime
  }
+
+ [int]update_timediff(){
+ [long]$servertime=$this.Get_Time()
+[long]$mytime=((New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds)
+ [int]$this.timediff =$mytime - $servertime
+ return $this.timediff
+ }
  
  $binanceapiserver=("api.binance.com",
 "api1.binance.com",
 "api2.binance.com",
 "api3.binance.com")
-
-[PSCustomObject]get([String] $function, [String] $data,[bool] $sendapikey, [bool] $signquery){ 
-if($data -eq $null){ $result=Invoke-Webrequest  "https://$($this.usedapiserver)/api/v3/$($function)";}
+$websocketserver="wss://stream.binance.com:9443"
+[PSCustomObject]action([String] $function, [String] $data,[bool] $sendapikey, [bool] $signquery,[String] $Method){ 
+if($null -eq $data){ $result=Invoke-Webrequest  "https://$($this.usedapiserver)/api/v3/$($function)";}
 else{
 if($signquery){
 $signature=$this.Sign($data)
-$result= (Invoke-Webrequest  -Method GET -Headers @{'X-MBX-APIKEY' = $($this.ApiKey)} "https://$($this.usedapiserver)/api/v3/$($function)?$data&signature=$signature")}
+$result= (Invoke-Webrequest  -Method $Method -Headers @{'X-MBX-APIKEY' = $($this.ApiKey)} "https://$($this.usedapiserver)/api/v3/$($function)?$data&signature=$signature")}
 else { if($sendapikey){$result= (Invoke-Webrequest -Headers @{'X-MBX-APIKEY' = $($this.ApiKey)} "https://$($this.usedapiserver)/api/v3/$($function)?$data")}else{$result= (Invoke-Webrequest  "https://$($this.usedapiserver)/api/v3/$($function)?$data") }}
 }
 Write-Host "Used 1m weight:$(($result.rawcontent -split "`n" -match "x-mbx-used-weight-1m" -split ":").item(1).trim())"
 return $result
 }
+[PSCustomObject]get([String] $function, [String] $data,[bool] $sendapikey, [bool] $signquery){ 
+ return ($this.action($function,$data,$sendapikey,$signquery,"GET"))
+}
 [PSCustomObject]get([String] $function, [String] $data,[bool] $sendapikey){ return ($this.get($function,$data,$true,$false))}
 [PSCustomObject]get([String] $function, [String] $data){ return ($this.get($function,$data,$false,$false))}
 [PSCustomObject]get([String] $function){ return ($this.get($function,"",$false,$false))}
+[PSCustomObject]post([String] $function, [String] $data,[bool] $sendapikey, [bool] $signquery){ 
+ return ($this.action($function,$data,$sendapikey,$signquery,"POST"))
+}
+[PSCustomObject]delete([String] $function, [String] $data,[bool] $sendapikey, [bool] $signquery){ 
+ return ($this.action($function,$data,$sendapikey,$signquery,"DELETE"))
+}
+
+
 [String]sign([String] $message){$hmacsha = New-Object System.Security.Cryptography.HMACSHA256
 $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($this.ApiSecret)
 $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes("$message"))
@@ -50,7 +69,7 @@ $StartTime = $(get-date)
 $ProgressPreference="SilentlyContinue"
  Invoke-Webrequest  "https://$apiserver/api/v3/ping" -Method Get | Out-Null
  $milliseconds=($(New-TimeSpan -Start $StartTime -End $(get-date))).Milliseconds
-  $serverresult= New-Object -TypeName "PSCustomObject"
+ $serverresult= New-Object -TypeName "PSCustomObject"
  $serverresult| Add-Member -type NoteProperty -Name "APIServer" -value $apiserver
  $serverresult| Add-Member -type NoteProperty -Name "ResponseTime" -value $milliseconds
  $resultlist+=$serverresult
@@ -66,7 +85,12 @@ $ProgressPreference="SilentlyContinue"
 [double]get_currency_balance([String] $symbol){
 [long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff 
 
-return (($this.get("account","timestamp=$timestamp",$true,$true) | ConvertFrom-Json).balances | Where { $_.asset -eq $symbol}).free
+return (($this.get("account","timestamp=$timestamp",$true,$true) | ConvertFrom-Json).balances | Where-Object { $_.asset -eq $symbol}).free
+}
+[PSCustomObject]get_balance(){
+[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff 
+
+return (($this.get("account","timestamp=$timestamp",$true,$true) | ConvertFrom-Json).balances)
 }
 [PSCustomObject]depth([String] $symbol, [int] $limit){ return ($this.get("depth","symbol=$Symbol&limit=$limit") | ConvertFrom-Json) }
 [PSCustomObject]depth([String] $symbol){ return ($this.get("depth","symbol=$Symbol") | ConvertFrom-Json) }
@@ -89,5 +113,15 @@ return (($this.get("account","timestamp=$timestamp",$true,$true) | ConvertFrom-J
 [PSCustomObject]myTrades([String] $symbol,[long] $recvWindow, [int]$limit,[long] $starttime, [long] $endtime ){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("myTrades","symbol=$Symbol&limit=$limit&timestamp=$timestamp&StartTime=$starttime&endtime=$endtime&recvWindow=$recvWindow",$true,$true) | ConvertFrom-Json) }
 [PSCustomObject]myTrades([String] $symbol,[long] $recvWindow, [int]$limit){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("myTrades","symbol=$Symbol&limit=$limit&timestamp=$timestamp&recvWindow=$recvWindow",$true,$true) | ConvertFrom-Json) }
 [PSCustomObject]myTrades([String] $symbol){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("myTrades","symbol=$Symbol&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]newLimitOrder([String] $symbol,[double] $quantity, [String] $ordertype, [String] $side, [double] $price, [string] $timeInForce ){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.post("order","symbol=$Symbol&type=$ordertype&side=$side&price=$($price -replace ",",".")&quantity=$($quantity -replace ",",".")&timeInForce=$timeInForce&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+#[PSCustomObject]newLimitOrder([String] $symbol,[double] $quantity, [String] $ordertype, [String] $side, [double] $price, [string] $timeInForce ){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.post("order","symbol=$Symbol&type=$ordertype&side=$side&price=$price&quantity=$quantity&timeInForce=$timeInForce&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+#[PSCustomObject]newMarketOrder([String] $symbol,[double] $quantity, [String] $ordertype, [String] $side, [double] $price, [string] $timeInForce ){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.post("order","symbol=$Symbol&type=$ordertype&side=$side&price=$price&quantity=$quantity&timeInForce=$timeInForce&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+#[PSCustomObject]newMarketOrder([String] $symbol,[double] $quantity, [String] $ordertype, [String] $side, [double] $price, [string] $timeInForce ){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.post("order","symbol=$Symbol&type=$ordertype&side=$side&price=$price&quantity=$quantity&timeInForce=$timeInForce&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]cancelOrder([String] $symbol,[long] $orderId){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.delete("order","symbol=$Symbol&timestamp=$timestamp&orderId=$orderId",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]queryOrder([String] $symbol,[string] $orderId){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("order","symbol=$Symbol&orderId=$orderId&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]cancelOpenOrders([String] $symbol){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.delete("openOrders","symbol=$Symbol&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]queryOpenOrders([String] $symbol){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("openOrders","symbol=$Symbol&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]queryOpenOrders(){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("openOrders","timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
+[PSCustomObject]allOrders([String] $symbol){[long]$timestamp=(New-TimeSpan -Start "01.01.1970" -End $(Get-Date)).TotalMilliSeconds - $this.timediff; return ($this.get("allOrders","symbol=$Symbol&timestamp=$timestamp",$true,$true) | ConvertFrom-Json) }
 
 }
