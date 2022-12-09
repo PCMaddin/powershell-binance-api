715fe5edc0bdb8717d855a4892dbc255e34d167b
PCMaddin <pcmaddin@gmail.com>
2022-12-09 19:14:34 +0100

    Update binance-api.ps1
    Adding Examples for limit order

--- binance-api.ps1
+++ binance-api.ps1
@@ -28,3 +28,18 @@ Pause(30)
 #$allticker=$bt.ticker24hr()
 #$bridge="BUSD"
 #($allticker| Where{ $_.symbol -like "*$bridge"  }| Where { $( [double]$_.quoteVolume) -gt 5000000}).symbol.replace("$bridge","") | Out-File supported_coinlist.txt
+
+##EXAMPLE FOR A ORDER TO BUY AT ACTUAL PRICE
+$tradepair ="BTCUSDT"
+$quantity = 0.001    #Number of Coins
+$buyprice=[DOUBLE]($bt.price_ticker($tradepair).price)
+$order=$bt.newLimitOrder($tradepair,$quantity,"LIMIT","BUY",$buyprice,"GTC")
+WHILE(!($bt.queryOrder("$tradepair",($order.orderID)).status -eq "FILLED")){Start-Sleep 1}
+
+##SAME JUST WITH USDT INSTEAD OF COIN
+$tradepair ="BTCUSDT"
+$balance=50
+$buyprice=[DOUBLE]($bt.price_ticker($tradepair).price)
+$quantity = $quantity=[Math]::Floor((($balance / $buyprice)*10000))/10000    #Number of Coins
+$order=$bt.newLimitOrder($tradepair,$quantity,"LIMIT","BUY",$buyprice,"GTC")
+WHILE(!($bt.queryOrder("$tradepair",($order.orderID)).status -eq "FILLED")){Start-Sleep 1}
